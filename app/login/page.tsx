8eea5fe95124701d18b06e071432840bf5c4f24a
ELAZNZ <e.zenouzzadeh@gmail.com>
2026-02-25 12:51:25 +0330

    update login page & add API

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,50 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function LoginPage() {
-  const handleLogin = (e: React.FormEvent) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const res = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          expiresInMins: 30,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error("نام کاربری یا رمز عبور اشتباه است.");
+      }
+
+      console.log(data);
+
+      localStorage.setItem("accessToken", data.accessToken);
+
+      router.push("/panel");
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -18,26 +60,40 @@ export default function LoginPage() {
         </div>
 
         <form onSubmit={handleLogin} className="flex flex-col gap-5">
+          {errorMessage && (
+            <div className="bg-red-50 text-red-500 p-3 rounded-xl">
+              {errorMessage}
+            </div>
+          )}
+
           <input
             type="text"
-            placeholder="ایمیل یا شماره تلفن"
-            className="w-full border border-gray-200 rounded-xl py-2 px-4 outline-none transition-all text-left placeholder:text-right"
-            onChange={() => {}}
+            placeholder="نام کاربری"
+            value={username}
+            className="w-full border border-gray-200 rounded-xl py-2 px-4 outline-gray-400 transition-all text-left placeholder:text-right"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
             required
           />
 
           <input
+            type="password"
             placeholder="رمز عبور"
-            className="w-full border border-gray-200 rounded-xl py-2 px-4 outline-none transition-all text-left placeholder:text-right"
-            onChange={() => {}}
+            value={password}
+            className="w-full border border-gray-200 rounded-xl py-2 px-4 outline-gray-400 transition-all text-left placeholder:text-right"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             required
           />
 
           <button
             type="submit"
-            className="cursor-pointer w-full bg-gray-400 rounded-xl outline-none py-2 px-4 hover:bg-gray-200"
+            disabled={isLoading}
+            className="cursor-pointer w-full bg-gray-400 rounded-xl outline-none py-2 px-4 hover:bg-gray-500"
           >
-            ورود
+            {isLoading ? "در حال ورود..." : "ورود"}
           </button>
         </form>
       </div>
